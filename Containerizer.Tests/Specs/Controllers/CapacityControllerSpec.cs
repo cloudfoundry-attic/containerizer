da48fd4d00ec36e2fdb5257bf13baf5148fd8cf8
John_Shahid <jvshahid@pivotal.io>
2015-04-21 18:15:25 -0400

    WIP: GardenWindows#Capacity
    [#92253000]
    
    Signed-off-by: Dave_Goddard <dgoddard@pivotal.io>

---
+++ Containerizer.Tests/Specs/Controllers/CapacityControllerSpec.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NSpec;
+using Containerizer.Controllers;
+using System.Web.Http;
+using Containerizer.Models;
+using System.Web.Http.Results;
+using IronFrame;
+using Containerizer.Services.Interfaces;
+using Containerizer.Services.Implementations;
+
+namespace Containerizer.Tests.Specs.Controllers
+{
+    class CapacityControllerSpec : nspec
+    {
+        private void describe_()
+        {
+            describe[Controller.Index] = () =>
+            {
+                CapacityController controller = null;
+
+                before = () =>
+                {
+                    controller = new CapacityController();
+                };
+
+                it["returns positive capacity for MemoryInBytes"] = () =>
+                {
+                    var capacity = controller.Index();
+                    capacity.MemoryInBytes.should_be_greater_than(0);
+                };
+
+                it["returns positive capacity for DiskInBytes"] = () =>
+                {
+                    var capacity = controller.Index();
+                    capacity.DiskInBytes.should_be_greater_than(0);
+                };
+
+                it["returns positive capacity for MaxContainers"] = () =>
+                {
+                    var capacity = controller.Index();
+                    capacity.MaxContainers.should_be_greater_than(0);
+                };
+            };
+        }
+    }
+}
---
+++ Containerizer/Controllers/CapacityController.cs
@@ -0,0 +1,73 @@
+﻿using IronFrame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Containerizer.Models;
+using Containerizer.Services.Interfaces;
+using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using Containerizer.Factories;
+
+namespace Containerizer.Controllers
+{
+    public class Capacity
+    {
+        [JsonProperty("memory_in_bytes")]
+        public ulong MemoryInBytes;
+
+        [JsonProperty("disk_in_bytes")]
+        public ulong DiskInBytes;
+
+        [JsonProperty("max_containers")]
+        public ulong MaxContainers;
+    }
+
+    public class CapacityController : ApiController
+    {
+        [Route("api/capacity")]
+        public Capacity Index()
+        {
+            MEMORYSTATUSEX memStatus = new MEMORYSTATUSEX();
+            if (!GlobalMemoryStatusEx(memStatus))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            var drive = new System.IO.DriveInfo(ContainerServiceFactory.GetContainerRoot());
+
+            return new Capacity
+            {
+                MemoryInBytes = memStatus.ullTotalPhys,
+                DiskInBytes = (ulong)drive.TotalSize,
+                MaxContainers = Int32.MaxValue,
+            };
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class MEMORYSTATUSEX
+        {
+            public uint dwLength;
+            public uint dwMemoryLoad;
+            public ulong ullTotalPhys;
+            public ulong ullAvailPhys;
+            public ulong ullTotalPageFile;
+            public ulong ullAvailPageFile;
+            public ulong ullTotalVirtual;
+            public ulong ullAvailVirtual;
+            public ulong ullAvailExtendedVirtual;
+            public MEMORYSTATUSEX()
+            {
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+        }
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+    }
+}
